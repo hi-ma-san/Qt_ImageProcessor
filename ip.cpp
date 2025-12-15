bf7ba77e20dba2eb189e00df57de132457b62b16
hi-ma-san <41143161@nfu.edu.tw>
2025-12-15 15:39:26 +0800

    Project finish

--- ip.cpp
+++ ip.cpp
@@ -1,7 +1,116 @@
 #include "ip.h"
+#include <QMenu>
+#include <QFileDialog>
+#include <QDebug>
+#include <QPixmap>
+#include <QMenuBar>
 
 IP::IP(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    setWindowTitle(tr("Image Processor"));
+    central = new QWidget();
+    mainLayout = new QHBoxLayout(central);
+    imgWin = new QLabel();
+    QPixmap *initPixmap = new QPixmap(300, 200);
+    initPixmap -> fill(QColor(255, 255, 255));
+    imgWin -> resize(300, 200);
+    imgWin -> setScaledContents(true);
+    imgWin -> setPixmap(*initPixmap);
+    mainLayout -> addWidget(imgWin);
+    setCentralWidget(central);
+    createActions();
+    createMenus();
+    createToolBars();
+}
 
 IP::~IP() {}
+
+void IP::createActions()
+{
+    openFileAction = new QAction(tr("開啟檔案&O"), this);
+    openFileAction -> setShortcut(tr("Ctrl + O"));
+    openFileAction -> setStatusTip(tr("開啟影像檔案"));
+    connect(openFileAction, SIGNAL(triggered(bool)), this, SLOT(showOpenFile()));
+
+    exitAction = new QAction(tr("結束&Q"), this);
+    exitAction -> setShortcut(tr("ctrl + Q"));
+    exitAction -> setStatusTip(tr("退出程式"));
+    connect(exitAction, SIGNAL(triggered(bool)), this, SLOT(close()));
+
+    zoomInAction = new QAction(tr("放大"), this);
+    zoomInAction -> setShortcut(tr("ctrl + ="));
+    connect(zoomInAction, SIGNAL(triggered(bool)), this, SLOT(windowZoomIn()));
+
+    zoomOutAction = new QAction(tr("縮小"), this);
+    zoomOutAction -> setShortcut(tr("ctrl + -"));
+    connect(zoomOutAction, SIGNAL(triggered(bool)), this, SLOT(windowZoomOut()));
+}
+
+void IP::createMenus()
+{
+
+    fileMenu = menuBar() -> addMenu(tr("檔案"));
+    fileMenu -> addAction(openFileAction);
+    fileMenu -> addAction(exitAction);
+
+    toolMenu = menuBar() -> addMenu(tr("工具"));
+    toolMenu -> addAction(zoomInAction);
+    toolMenu -> addAction(zoomOutAction);
+}
+
+void IP::createToolBars()
+{
+    fileTool = addToolBar("file");
+    fileTool -> addAction(openFileAction);
+
+    toolBar = addToolBar("tool");
+    toolBar -> addAction(zoomInAction);
+    toolBar -> addAction(zoomOutAction);
+}
+
+void IP::loadFile(QString filename)
+{
+    qDebug() << QString("file name:%1").arg(filename);
+    QByteArray ba = filename.toLatin1();
+    printf("FN:%s\n", (char *) ba.data());
+    img.load(filename);
+    imgWin -> setPixmap(QPixmap::fromImage(img));
+}
+
+void IP::showOpenFile()
+{
+    filename = QFileDialog::getOpenFileName(this, tr("開啟影像"), tr("."), "bmp(*.bmp);;png(*.png);;Jpeg(*.jpg)");
+    if (!filename.isEmpty())
+    {
+        if(img.isNull())
+        {
+            loadFile(filename);
+        }
+        else
+        {
+            IP *newIPWin = new IP();
+            newIPWin -> show();
+            newIPWin -> loadFile(filename);
+        }
+    }
+}
+
+void IP::windowZoomOut()
+{
+    QImage zoomOuted;
+    zoomOuted = img.scaled(img.width() / 2, img.height() / 2);
+    QLabel *ret = new QLabel();
+    ret -> setPixmap(QPixmap::fromImage(zoomOuted));
+    ret -> show();
+}
+
+void IP::windowZoomIn()
+{
+    QImage zoomOuted;
+    zoomOuted = img.scaled(img.width() * 2, img.height() * 2);
+    QLabel *ret = new QLabel();
+    ret -> setPixmap(QPixmap::fromImage(zoomOuted));
+    ret -> show();
+}
+
--- ip.h
+++ ip.h
@@ -2,6 +2,12 @@
 #define IP_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QImage>
+#include <QLabel>
+#include <QHBoxLayout>
 
 class IP : public QMainWindow
 {
@@ -10,5 +16,30 @@ class IP : public QMainWindow
 public:
     IP(QWidget *parent = nullptr);
     ~IP();
+
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void loadFile(QString filename);
+
+private:
+    QWidget     *central;
+    QMenu       *fileMenu;
+    QMenu       *toolMenu;
+    QToolBar    *fileTool;
+    QToolBar    *toolBar;
+    QImage      img;
+    QString     filename;
+    QLabel      *imgWin;
+    QAction     *openFileAction;
+    QAction     *exitAction;
+    QAction     *zoomInAction;
+    QAction     *zoomOutAction;
+    QHBoxLayout *mainLayout;   // ★ 存 layout
+
+private slots:
+    void showOpenFile();
+    void windowZoomIn();
+    void windowZoomOut();
 };
 #endif // IP_H
